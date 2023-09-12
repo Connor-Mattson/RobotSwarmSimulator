be26cafece919d223e64ba0f9bcd36c36265e154
Connor Mattson <connor.g.mattson@gmail.com>
2023-09-12 11:13:31 -0600

    Post-Processing ICRA Experiments

--- demo/evolution/optim_milling/sim_results.py
+++ demo/evolution/optim_milling/sim_results.py
@@ -62,54 +62,43 @@ def get_world_generator(n_agents, horizon):
 
     return gene_to_world
 
+def m_per_s_to_pixels_per_second(genome):
+    genome[0] *= (100 / 15) * SCALE
+    genome[2] *= (100 / 15) * SCALE
+    return genome
+
 
 if __name__ == "__main__":
+    """
+    Example usage:
+    `python -m demo.evolution.optim_milling.sim_results --v0 0.1531 --w0 0.3439 --v1 0.1485 --w1 0.1031 --n 10 --t 1000`
+    """
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--name", type=str, help="Name of the experiment", default=None)
-    parser.add_argument("--root", type=str, help="Experiment folder root", default=None)
     parser.add_argument("--n", type=int, default=10, help="Number of agents")
     parser.add_argument("--t", type=int, default=1000, help="Environment Horizon")
-    parser.add_argument("--processes", type=int, default=1, help="Number of running concurrent processes")
-    parser.add_argument("--iters", type=int, default=None, help="Number of Evolutions to consider")
-    parser.add_argument("--pop-size", type=int, default=15, help="The size of each generation's population")
-    parser.add_argument("--sweep", action="store_true", help="Whether to sweep instead of search")
+    parser.add_argument("--v0", type=float, help="Forward_Speed_0", default=None)
+    parser.add_argument("--w0", type=float, help="Turning_Rate_0", default=None)
+    parser.add_argument("--v1", type=float, help="Forward_Speed_1", default=None)
+    parser.add_argument("--w1", type=float, help="Turning_Rate_1", default=None)
+    parser.add_argument("--no-stop", action="store_true", help="Whether to stop at T limit or not")
 
     args = parser.parse_args()
 
-    exp = Experiment(root="demo/results/out" if not args.root else args.root, title=args.name)
-
-    # Fix the initial conditions if params indicate
-    # init = None
-    # if args.fixed_config:
-    #     init = FixedInitialization("demo/configs/flockbots-icra/init_translated.csv")
-
     # Save World Config by sampling from generator
     world_gen_example = get_world_generator(args.n, args.t)
-    sample_worlds = world_gen_example([-1, -1, -1, -1], [-1, -1, -1, -1])
-    # sample_worlds[0].stop_at = 1003
-    # sim(world_config=sample_worlds[0], save_every_ith_frame=2, save_duration=1000)
 
-    sample_worlds[0].save_yaml(exp)
+    translated_genome = m_per_s_to_pixels_per_second([args.v0, args.w0, args.v1, args.w1])
+    sample_worlds = world_gen_example(translated_genome, [-1, -1, -1, -1])
 
-    cmaes = CMAES(
-        FITNESS,
-        genome_to_world=get_world_generator(args.n, args.t),
-        dvars=DECISION_VARS,
-        num_processes=args.processes,
-        show_each_step=False,
-        target=PERFECT_CIRCLE_SCORE,
-        experiment=exp,
-        max_iters=args.iters,
-        pop_size=args.pop_size,
-    )
-    if args.sweep:
-        cmaes.sweep_parameters([7, 7, 7, 7])
+    if args.no_stop:
+        sample_worlds[0].stop_at = None
     else:
-        cmaes.minimize()
+        sample_worlds[0].stop_at = args.t
 
-
-
-"""
-Exact Icra Command: python -m demo.evolution.optim_milling.milling_search --name "test_mill_optim" --n 10 --t 1000 --processes 15 --pop-size 15 --iters 100
-"""
+    w = sim(world_config=sample_worlds[0], save_every_ith_frame=2, save_duration=1000)
+    try:
+        print(f"Final Circliness: {w.behavior[0].out_average()[1]}")
+    except:
+        pass
