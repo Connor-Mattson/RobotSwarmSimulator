67d96a64ba781914b24974dc7cec5850d89ada60
varunraveendra <56466294+varunraveendra@users.noreply.github.com>
2025-02-22 17:24:19 -0700

    Update CircularCollider.py
    removed redundant code

--- src/novel_swarms/util/collider/CircularCollider.py
+++ src/novel_swarms/util/collider/CircularCollider.py
@@ -29,32 +29,21 @@ class CircularFrictionCollider:
     def collision_then_correction(self, other):
         dist_between_radii = self.dist(other)
         dist_difference = (self.r + other.r) - dist_between_radii
-        #collide_point = np.array([self.x + (other.x-self.x)/2, self.y +(other.y-self.y)/2])
         collide_point_vector= other.v - self.v
         correction_vector = None
-        #print("emtry",self.old_collide_agent)
         if dist_difference < 0 and not self.old_collide_agent:
             return None,None,None
         if dist_difference > 0:
             correction_vector = ((other.v - self.v) / (dist_between_radii + 0.001)) * (dist_difference + 0.01)
-        #print(correction_vector)
-        #print("right")
-        other_dir = 0#self.get_direction_angle_coeff(collide_point_vector,other)
+        
         self_dir = self.get_direction_angle_coeff(collide_point_vector)
-        other_mag =1# np.linalg.norm(np.array([other.dx,other.dy,0]))
-        self_mag =1# np.linalg.norm(np.array([self.dx,self.dy,0]))
         if correction_vector is None:
-            other_dir=0
             self_dir=0
-        angle = self.da+self.da*(1.12*-self_dir)- self.da*0.003 #self.robot_friction*(((self_dir/(np.pi/2))*self_mag*1)-((other_dir/(np.pi/2))*other_mag*0.5)) # (equal and opp reaction) - (external reaction)
-        #print(angle)
-        #print("correct ang",self.da,other_dir,self_dir)
+        angle = self.da+self.da*(1.12*-self_dir)- self.da*0.003 # constants in this line are tuned after real value experiments
         if correction_vector is not None:
-            self.old_collide_agent= False #changing this to false was not tested, but seems to work
-            #print("here")
+            self.old_collide_agent= False 
         else:
             self.old_collide_agent= True
-        #print("check",self.old_collide_agent)
         return correction_vector,angle,self.old_collide_agent
     
     def get_direction_angle_coeff(self,collide_point_vector,agent=None):
